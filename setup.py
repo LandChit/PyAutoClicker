13165d540f337ed4cf4676e91890e46d5c507837
LandChit <78423278+LandChit@users.noreply.github.com>
2022-01-08 21:05:11 +0800

    made setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='PyAutoClicker',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/LandChit/PyAutoClicker',
+    license='',
+    author='LandChit',
+    author_email='',
+    description='Landchits original autoclicker well its technically unoriginal'
+)
